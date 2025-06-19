a98f359f208f24294998643ddcdfa2bb03424be2
diegofs29 <diegofdezsrz12@gmail.com>
2025-06-19 21:56:00 +0200

    feat(pitch): implementar pruebas del elevator pitch

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,21 @@
 RSpec.describe "Main page", type: :feature, js: true do
-  it "has the page title" do
-    visit '/'
-    # `binding.pry` is useful for crafting the right selector
-    # or checking the actual state of the page
-    # binding.pry # test will pause here
-    expect(find('#title').text).to eq('Diego Fernández Suárez')
+  context "when reading the header" do
+    it "should have the page title" do
+      visit '/'
+      expect(find('#title').text).to eq('Diego Fernández Suárez')
+    end
+  end
+
+  context "when reading elevator pitch" do
+    it "should have a welcome title" do
+      visit '/'
+      expect(find('#pitch-welcome').text).to eq('Welcome / Bienvenidos / Bienveníos')
+    end
+
+    it "should have a paragraph" do 
+      visit '/'
+      expect(find('#pitch-paragraph').text).to start_with '¡Hola! My name is Diego'
+      expect(find('#pitch-paragraph').text).to end_with 'world wide web!'
+    end
   end
 end
