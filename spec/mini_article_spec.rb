a1cc80b444753505e8db060ddf534a163db0bd12
diegofs29 <diegofdezsrz12@gmail.com>
2025-06-23 22:58:55 +0200

    feat(mini-article): implementar pruebas de miniaturas de artículos

---
+++ spec/mini_article_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe "Article miniatures", type: :feature, js: true do
+  context "when reading the title" do
+    it "should be Articles" do
+      visit '/'
+      expect(find('#articles-title').text).to eq('Articles')
+    end
+  end
+
+  context "when counting the articles" do
+    it "should be greater than 0" do
+      visit '/'
+      scroll_to(find('#articles-list'))
+      expect(find('#articles-list')).to have_css('.dotless')
+      expect(find('#articles-list')).to have_css('.article-card')
+    end
+  end
+end
