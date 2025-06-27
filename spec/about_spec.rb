37e371b8dbb4bafbbdcd73800aacde15a575179a
diegofs29 <diegofdezsrz12@gmail.com>
2025-06-27 20:11:22 +0200

    feat(about): completar tests con un caso mas

--- spec/about_spec.rb
+++ spec/about_spec.rb
@@ -4,5 +4,9 @@ RSpec.describe "About me", type: :feature, js: true do
       visit '/about.html'
       expect(find('#title').text).to eq('About me')
     end
+    it "paragraph element should exist" do
+      visit 'about.html'
+      expect(page.body).to include(/(?m)((<p>)(.*?)(<\/p>))+/)
+    end
   end
 end
