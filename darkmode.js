c7cd064056ee80da580ae11b92b0625d65b298ce
diegofs29 <diegofdezsrz12@gmail.com>
2025-06-30 20:40:07 +0200

    feat(contact): añadir iconos de contacto

--- darkmode.js
+++ darkmode.js
@@ -2,9 +2,6 @@ var root = document.documentElement,
   theme = window.getComputedStyle(root)
     .getPropertyValue('--light') === ' ' ? 'dark' : 'light';
 
-document.getElementById('toggle-theme')
-  .addEventListener('click', toggleTheme);
-
 function toggleTheme() {
   root.classList.remove(theme);
   theme = (theme === 'dark') ? 'light' : 'dark';
