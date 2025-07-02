fd6bb088f11e9690298d23b753c17b9c25fe700a
diegofs29 <diegofdezsrz12@gmail.com>
2025-07-02 21:56:41 +0200

    fix: cambiar it de los test sobre los artículos

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe "Article", type: :feature, js: true do
   context "when reading the title" do
-    it "first post should exist" do
+    it "first post should be Las 24 horas de Le Mans 2025" do
       visit '/blog.html'
       find('#post-item1-url').click
       expect(find('#post-title').text).to eq('Las 24 horas de Le Mans 2025')
     end
 
-    it "second post should exist" do 
+    it "second post should be La Mandarina: Un Cítrico Dulce y Aromático para Disfrutar" do 
       visit '/blog.html'
       find('#post-item2-url').click
       expect(find('#post-title').text).to eq('La Mandarina: Un Cítrico Dulce y Aromático para Disfrutar')
