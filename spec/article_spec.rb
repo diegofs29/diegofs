fed02e50a9e92166945927a90a2b2cfd335d6f0a
diegofs29 <diegofdezsrz12@gmail.com>
2025-07-02 21:37:52 +0200

    fix: implementar correctamente las pruebas de los artículos

--- spec/article_spec.rb
+++ spec/article_spec.rb
@@ -1,10 +1,15 @@
 RSpec.describe "Article", type: :feature, js: true do
   context "when reading the title" do
-    it "should exist" do
+    it "first post should exist" do
       visit '/blog.html'
-      scroll_to(find('#articles-list'))
-      expect(find('#articles-list')).to have_css('.dotless')
-      expect(find('#articles-list')).to have_css('.article-card')
+      find('#post-item1-url').click
+      expect(find('#post-title').text).to eq('Las 24 horas de Le Mans 2025')
+    end
+
+    it "second post should exist" do 
+      visit '/blog.html'
+      find('#post-item2-url').click
+      expect(find('#post-title').text).to eq('La Mandarina: Un Cítrico Dulce y Aromático para Disfrutar')
     end
   end
 end
